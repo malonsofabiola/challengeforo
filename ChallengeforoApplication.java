dfafe31694484a176adb0133715877444d862c93
malonsofabiola <168011171+malonsofabiola@users.noreply.github.com>
2024-07-16 12:34:25 -0600

    Se actualiza proyecto

---
+++ ChallengeforoApplication.java
@@ -0,0 +1,13 @@
+package aluracursos.challengeforo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeforoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeforoApplication.class, args);
+	}
+
+}
---
+++ ForoController.java
@@ -0,0 +1,9 @@
+package aluracursos.challengeforo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/topicos")
+public class ForoController {
+}
